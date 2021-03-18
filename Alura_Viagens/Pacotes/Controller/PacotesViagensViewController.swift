eeb9c437e19fd67f0a7030eb5c52411e21ef125d
KinhoA3M <erick.a.menezes31@gmail.com>
2021-03-18 16:29:53 -0300

    Commit final aula 1 curso layouts 2

---
+++ Alura_Viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -0,0 +1,48 @@
+//
+//  PacotesViagensViewController.swift
+//  Alura_Viagens
+//
+//  Created by Erick Avila Menezes on 18/03/21.
+//
+
+import UIKit
+
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
+    
+    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        colecaoPacotesViagem.dataSource = self
+        colecaoPacotesViagem.delegate = self
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return self.listaViagens.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let celulaPacote = collectionView.dequeueReusableCell(withReuseIdentifier: "celulaPacote", for: indexPath) as! PacoteViagemCollectionViewCell
+        
+        let viagemAtual = listaViagens[indexPath.item]
+        
+        celulaPacote.labelTitulo.text = viagemAtual.titulo
+        celulaPacote.labelQuantidadeDeDias.text = "\(viagemAtual.quantidadeDeDias) dias"
+        celulaPacote.labelPreco.text = "R$ \(viagemAtual.preco)"
+        celulaPacote.imagemViagem.image = UIImage(named: viagemAtual.caminhoDaImagem)
+        
+        celulaPacote.layer.borderWidth = 0.5
+        celulaPacote.layer.borderColor = UIColor(red: 85.0/255.0, green: 85.0/255.0, blue: 85.0/255.0, alpha: 1).cgColor
+        celulaPacote.layer.cornerRadius = 8
+
+        return celulaPacote
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let larguraCelula = collectionView.bounds.width / 2
+        return CGSize(width: larguraCelula-15, height: 160)
+    }
+    
+}
---
+++ Alura_Viagens/Pacotes/View/PacoteViagemCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  PacoteViagemCollectionViewCell.swift
+//  Alura_Viagens
+//
+//  Created by Erick Avila Menezes on 18/03/21.
+//
+
+import UIKit
+
+class PacoteViagemCollectionViewCell: UICollectionViewCell {
+
+    @IBOutlet weak var imagemViagem: UIImageView!
+    
+    @IBOutlet weak var labelTitulo: UILabel!
+    
+    @IBOutlet weak var labelQuantidadeDeDias: UILabel!
+    
+    @IBOutlet weak var labelPreco: UILabel!
+}
+
