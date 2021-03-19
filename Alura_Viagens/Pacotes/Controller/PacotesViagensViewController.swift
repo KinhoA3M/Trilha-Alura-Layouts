7304a0eb7bc86b21ee9041778e0a9fe7f0e58281
KinhoA3M <erick.a.menezes31@gmail.com>
2021-03-19 10:48:32 -0300

    Commit LAyouts 2 modulo, aula 2 fim

--- Alura_Viagens/Pacotes/Controller/PacotesViagensViewController.swift
+++ Alura_Viagens/Pacotes/Controller/PacotesViagensViewController.swift
@@ -7,16 +7,25 @@
 
 import UIKit
 
-class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+class PacotesViagensViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout, UISearchBarDelegate {
     
     @IBOutlet weak var colecaoPacotesViagem: UICollectionView!
     
-    let listaViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    @IBOutlet weak var pesquisarViagens: UISearchBar!
+    
+    let listaComTodasViagens: Array<Viagem> = ViagemDAO().retornaTodasAsViagens()
+    
+    @IBOutlet weak var labelContadorDePacotes: UILabel!
+    
+    var listaViagens: Array<Viagem> = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        listaViagens = listaComTodasViagens
         colecaoPacotesViagem.dataSource = self
         colecaoPacotesViagem.delegate = self
+        pesquisarViagens.delegate = self
+        self.labelContadorDePacotes.text = self.atualizaContadorLabel()
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -43,6 +52,20 @@ class PacotesViagensViewController: UIViewController, UICollectionViewDataSource
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let larguraCelula = collectionView.bounds.width / 2
         return CGSize(width: larguraCelula-15, height: 160)
+    }
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        listaViagens = listaComTodasViagens
+        if searchText != "" {
+            let filtroListaViagem = NSPredicate(format: "titulo contains %@", searchText)
+            let listaFiltrada: Array<Viagem> = (listaViagens as NSArray).filtered(using: filtroListaViagem) as! Array
+            listaViagens = listaFiltrada
+        }
+        self.labelContadorDePacotes.text = self.atualizaContadorLabel()
+        colecaoPacotesViagem.reloadData()
+    }
+    
+    func atualizaContadorLabel() -> String {
+        return listaViagens.count == 1 ? "1 pacote wncontrado" : "\(listaViagens.count) pacotes encontrados"
     }
     
 }
